d9e1d5c03c171a92001042c9de5b752d02f3c92f
nalshaamy <nalshaamy@gmail.com>
2026-07-18 20:07:08 +0300

    Delete flexsys_operations directory

--- flexsys_operations/common/exceptions.py
+++
@@ -1,9 +0,0 @@
-"""Domain exceptions used by FlexSys services."""
-
-
-class FlexSysError(Exception):
-    """Base exception for controlled FlexSys domain errors."""
-
-
-class FlexSysValidationError(FlexSysError):
-    """Raised when domain input fails validation."""
