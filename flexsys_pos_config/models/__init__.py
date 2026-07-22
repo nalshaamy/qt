b6da0e9b66d4098e802e76e9282b9541e7dc5e33
nalshaamy <nalshaamy@gmail.com>
2026-07-22 17:52:58 +0300

    Delete flexsys_pos_config directory

--- flexsys_pos_config/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# FLPOS models package.
-
-from . import pos_config
-from . import pos_session
-from . import receipt_block
-from . import receipt_template
-from . import res_config_settings
