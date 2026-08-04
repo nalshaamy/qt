8d58ddd3ccdeb4b8214b81ed47826d0609628f3f
nalshaamy <nalshaamy@gmail.com>
2026-08-04 16:54:14 +0300

    Delete pos_bonat_loyalty directory

--- pos_bonat_loyalty/controllers/__init__.py
+++
@@ -1,4 +0,0 @@
-# coding: utf-8
-
-from . import binary
-from . import main
--- pos_bonat_loyalty/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-
-from . import ir_http
-from . import pos_config
-from . import pos_session
-from . import res_company
-from . import res_config_settings
