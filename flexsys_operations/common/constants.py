dfb8527de10e498e25d61d8c30fe361f775ed751
nalshaamy <nalshaamy@gmail.com>
2026-08-04 21:47:11 +0300

    Delete flexsys_operations directory

--- flexsys_operations/common/constants.py
+++
@@ -1,8 +0,0 @@
-"""Shared constants for FlexSys Operations.
-
-Keep this module free of Odoo environment access so constants remain easy to reuse
-and test.
-"""
-
-MODULE_TECHNICAL_NAME = "flexsys_operations"
-MODULE_DISPLAY_NAME = "FlexSys"
--- flexsys_operations/services/notification_service.py
+++
@@ -1,9 +0,0 @@
-"""NotificationService application service."""
-
-from .base_service import BaseService
-
-
-class NotificationService(BaseService):
-    """Reserved service boundary; business logic will move here incrementally."""
-
-    pass
