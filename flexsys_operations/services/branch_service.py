1f36b3e1155020723c99172bab301011a4dafc2f
nalshaamy <nalshaamy@gmail.com>
2026-08-05 04:04:39 +0300

    Delete flexsys_operations directory

--- flexsys_operations/services/branch_service.py
+++
@@ -1,9 +0,0 @@
-"""BranchService application service."""
-
-from .base_service import BaseService
-
-
-class BranchService(BaseService):
-    """Reserved service boundary; business logic will move here incrementally."""
-
-    pass
